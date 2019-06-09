26752d68e9d7b572405611c112211ceb80c9bf33
Maciej Moroz <maciej.moroz89@gmail.com>
2019-06-09 20:04:19 +0200

    refactor

--- react-shop-redux/src/pages/Catalog/CatalogPage.js
+++ react-shop-redux/src/pages/Catalog/CatalogPage.js
@@ -1,58 +1,3 @@
-// import React, { useEffect } from "react";
-
-// import HeaderBig from "components/Header/HeaderBig";
-// import ProductsList from "components/ProductsList/ProductsList";
-// import styles from "./CatalogPage.module.css";
-// // import Filters from "./components/Filters/Filters";
-
-// const CatalogPage = ({
-//   product,
-//   isLoading,
-//   isError,
-//   fetchProductsWithRedux,
-//   addProduct
-// }) => {
-//   useEffect(() => {
-//     fetchProductsWithRedux();
-//   }, []);
-
-//   if (isError) {
-//     return <h2>Error while loading... :(</h2>;
-//   }
-
-//   if (isLoading) {
-//     return <h2>Loading...</h2>;
-//   }
-
-//   // let handleFilterChange = product => {
-//   //   let fitredProduct;
-
-//   //   return fitredProduct;
-//   // };
-
-//   return (
-//     <>
-//       <HeaderBig>Catalog</HeaderBig>
-
-//       <div className={styles.Catalog}>
-//         <div className={styles.ColumnLeft}>
-//           {/* <Filters
-//             initValue={product.initFilters}
-//             onChange={handleFilterChange}
-//             manufacturers={product.manufacturers}
-//           /> */}
-//         </div>
-
-//         <div className={styles.ColumnRight}>
-//           <ProductsList product={product} />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default CatalogPage;
-
 import React, { useEffect, useState } from "react";
 
 import HeaderBig from "components/Header/HeaderBig";
@@ -68,10 +13,12 @@ const CatalogPage = ({
   fetchProductsWithRedux,
   uniqueManufacturers
 }) => {
-  useEffect(() => {
-    fetchProductsWithRedux();
-    // eslint-disable-next-line
-  }, []);
+  // w przypadku ctr+r na catalogPage nie wyswuetla danych czy use effect wywalić czy zostawic ?
+
+  // useEffect(() => {
+  //   fetchProductsWithRedux();
+  //   // eslint-disable-next-line
+  // }, []);
 
   const [name, searchInput] = useState("");
   const [manufacture, changeInput] = useState("");
